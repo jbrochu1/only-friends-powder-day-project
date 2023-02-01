d7f1bd23dcc7a17283a6ea6cf0c2d13c2f8b96a0
jbrochu1 <111677770+jbrochu1@users.noreply.github.com>
2023-02-01 14:11:22 -0700

    cleaned up models comments

--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -1,5 +1,7 @@
 class Mountain < ApplicationRecord
     has_many :trips
+    
+    # FOR FUTURE USE
     # has_many :mountain_trips
     # has_many :trips, through: :mountain_trips
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,9 +1,11 @@
 class Trip < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :users, through: :comments
-  # has_many :mountain_trips
-  # has_many :mountains, through: :mountain_trips
   has_many :user_trips, dependent: :destroy
   has_many :users, through: :user_trips
   belongs_to :mountain
+
+  # FOR FUTURE USE
+  # has_many :mountain_trips
+  # has_many :mountains, through: :mountain_trips
 end
